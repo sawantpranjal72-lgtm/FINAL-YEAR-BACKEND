23584da80c1d4e12f1c74a4ab3eb19c8d001ca10
sawantpranjal72-lgtm <sawantpranjal72@gmail.com>
2026-04-04 14:26:12 +0530

    Update WebConfig.java

--- src/main/java/com/digitalfir/config/WebConfig.java
+++ src/main/java/com/digitalfir/config/WebConfig.java
@@ -8,21 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // ✅ CORS CONFIG
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500",
-                "https://final-year-backend-production-1d3f.up.railway.app",
-                "https://final-year-frontend-mu.vercel.app"
-             )
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5500",
+                   "https://final-year-backend-production-1d3f.up.railway.app",
+                        "https://final-year-frontend-mu.vercel.app"
+                            )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 
-    // Static resource setup
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")

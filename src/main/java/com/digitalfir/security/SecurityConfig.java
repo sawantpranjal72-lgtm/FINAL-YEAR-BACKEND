43d22db326baf7dbf9a4c55be3adb1117960c25b
sawantpranjal72-lgtm <sawantpranjal72@gmail.com>
2026-04-04 14:36:43 +0530

    Update SecurityConfig.java

--- src/main/java/com/digitalfir/security/SecurityConfig.java
+++ src/main/java/com/digitalfir/security/SecurityConfig.java
@@ -101,24 +101,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // 🔥 FINAL CORS CONFIG (MAIN FIX)
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of(
-                "https://final-year-frontend-mu.vercel.app"
-        ));
-
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
-    }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {

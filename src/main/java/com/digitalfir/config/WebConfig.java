e5b6721f356151f9f4cc82f770787adbc2ce6824
sawantpranjal72-lgtm <sawantpranjal72@gmail.com>
2026-04-04 13:22:16 +0530

    Update WebConfig.java

--- src/main/java/com/digitalfir/config/WebConfig.java
+++ src/main/java/com/digitalfir/config/WebConfig.java
@@ -12,7 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedOrigins(
+                "http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "https://final-year-backend-production-1d3f.up.railway.app"
+            )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
